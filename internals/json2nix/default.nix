d30a1093a36e5a0f32d5e22bf50883c0066fd488
h7x4 <h7x4@nani.wtf>
2022-11-26 21:08:14 +0100

    json2nix: `ghc942` -> `ghc924`
    Downgrade ghc compiler to support `nixos-22.05`

--- internals/json2nix/default.nix
+++ internals/json2nix/default.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 let
   overlayedPkgs = pkgs.extend
-    (pkgs.callPackage ./haskell-overlay.nix { compiler = "ghc942"; });
+    (pkgs.callPackage ./haskell-overlay.nix { compiler = "ghc924"; });
 in overlayedPkgs.writers.writeHaskellBin "json2nix" {
   libraries = with overlayedPkgs.haskellPackages; [ aeson nixfmt extra text ];
 } (builtins.readFile ./json2nix.hs)
