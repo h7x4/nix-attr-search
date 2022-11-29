3d92480602797fa46f0cee884607b3a3f98bebe8
h7x4 <h7x4@nani.wtf>
2022-11-29 16:16:47 +0100

    shell.nix: print aliases on starting nix-shell

--- shell.nix
+++ shell.nix
@@ -10,5 +10,11 @@ pkgs.mkShell {
     alias nasf=${format}/bin/nix-attr-search-format
     alias nashl=${hlint}/bin/nix-attr-search-hlint
     alias nashf=${ormolu}/bin/nix-attr-search-ormolu
+    cat << EOF
+    [Aliases]
+    nasf -> formats all nix files
+    nashl -> lint report for all haskell files
+    nashf -> format all haskell files
+    EOF
   '';
 }
